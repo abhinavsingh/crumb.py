20c3a8db7c2cb3bd2692f50854a52959e08821aa
Abhinav Singh <mailsforabhinav@gmail.com>
2014-04-26 22:36:14 -0700

    refactor, tests

--- README.md
+++ README.md
@@ -12,9 +12,12 @@ Usage
 >>> # generate crumb for user that expires in 30 seconds
 >>> c = Crumb('tim@tom.com', 30, secret='!@#$')
 >>> c.generate()
->>> print c.key
 830b60fe77b2
 >>>
+>>> # key attribute also contains the generated crumb
+>>> print c.key
+830b60fe77b2
+>>> 
 >>> # verify the generated crumb
 >>> c = Crumb('tim@tom.com', 30, secret='!@#$', key='830b60fe77b2')
 >>> c.validate()
--- crumb.py
+++ crumb.py
@@ -4,7 +4,7 @@ import hmac
 import random
 import hashlib
 
-VERSION = (0, 2)
+VERSION = (0, 3)
 __version__ = '.'.join(map(str, VERSION[0:2]))
 __description__ = 'Generate TTL based self expiring crumbs (token).'
 __author__ = 'Abhinav Singh'
@@ -17,6 +17,15 @@ class Crumb(object):
     choices = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
     
     def __init__(self, uid, ttl, key=None, action=None, secret=None):
+        '''Initialize crumb object.
+        
+        `uid`:    (string) unique id to associate with the crumbs
+        `ttl`:    (integer) generated crumbs will expire after ttl seconds
+        `key`:    (string) this is the generated crumb from previous run (pass for validation)
+        `action`: (string) generate several crumbs for same `uid`, `action` defines the context
+        `secret`: (string) a secret to use for crumb generation. It is generally a good idea to
+                  use a different secret for different `uid` used.
+        '''
         self.uid = str(uid)
         self.ttl = int(ttl)
         self.key = str(key) if key else key
@@ -24,28 +33,37 @@ class Crumb(object):
         self.secret = str(secret) if secret else self.gen_secret()
     
     def gen_secret(self):
+        '''Ramdomly generate a secret key.'''
         return ''.join([random.SystemRandom().choice(self.choices) for _ in range(50)])
     
     def challenge(self, i):
+        '''Calculate challenge for passed interval id `i`.'''
         return hmac.new(self.secret, '%s%s%s' % (i, self.action, self.uid), hashlib.sha256).hexdigest()[-12:]
     
+    def get_current_interval_challenge(self):
+        '''Get ongoing interval challenge.'''
+        i = self.get_current_interval_id()
+        return self.challenge(i)
+    
+    def get_previous_interval_challenge(self):
+        '''Get previous / passed interval challenge.'''
+        i = self.get_current_interval_id()
+        return self.challenge(i-1)
+    
+    def get_current_interval_id(self):
+        '''Get current time interval id.'''
+        return math.ceil(time.time() / int(self.ttl))
+    
     def validate(self):
+        '''Validate passed crumb key.'''
         assert self.key is not None
-        
-        t = time.time()
-        i = math.ceil(t / int(self.ttl))
-        
-        c = self.challenge(i)
-        if c == self.key:
+        if self.get_current_interval_challenge() == self.key \
+        or self.get_previous_interval_challenge() == self.key:
             return True
-        
-        c = self.challenge(i-1)
-        if c == self.key:
-            return True
-        
         return False
     
     def generate(self):
-        t = time.time()
-        i = math.ceil(t / int(self.ttl))
+        '''Generate a new crumb key.'''
+        i = self.get_current_interval_id()
         self.key = self.challenge(i)
+        return self.key
